9ddb4760426ba8316c2a11110bb336df1031eff8
Kacper Zurawski <kaczur190@gmail.com>
2022-12-13 09:46:15 +0100

    Update for payments

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL'),
+    credentials: true
+  });
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   await app.listen(process.env.APP_PORT, () =>
     console.log(`🚀 Server is listening on ${process.env.APP_PORT}`),
